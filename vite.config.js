b36442ed02f24eb2bb8ab92173a3d0f29fdf4ea7
mickael.maciel <mickael.maciel@gmail.com>
2025-09-05 11:13:06 -0300

    Feat vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/suporte-cw/',
-  optimizeDeps: {
-    exclude: ['file-saver', 'jszip'],
+  // base: '/', // no Vercel não precisa alterar base
+  build: {
+    outDir: 'dist'
   },
+  // Evita otimização esquisita de alguns workers/libs
+  optimizeDeps: {
+    exclude: []
+  }
 })
