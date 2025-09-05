52f1a8dd81bb25d03ca225b3c713c89252abc2b9
mickael.maciel <mickael.maciel@gmail.com>
2025-09-05 11:22:05 -0300

    feat:vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Vercel injeta a env VERCEL=1 nas builds
+const isVercel = process.env.VERCEL === '1'
+
 export default defineConfig({
   plugins: [react()],
-  // base: '/', // no Vercel não precisa alterar base
-  build: {
-    outDir: 'dist'
-  },
-  // Evita otimização esquisita de alguns workers/libs
-  optimizeDeps: {
-    exclude: []
-  }
+  base: isVercel ? '/' : '/suporte-cw/',  // Vercel -> '/', GitHub Pages -> '/suporte-cw/'
 })
